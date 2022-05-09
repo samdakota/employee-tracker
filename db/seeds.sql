9be97b20926026fea70894087d1133ddb9a06421
Sam Miller <sam.dakota@gmail.com>
2022-05-08 22:37:11 -0400

    fixed seeds.sql issue

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ('title', '100', 1)
+    ('title', 100, 1)
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
